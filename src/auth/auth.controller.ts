492437044b357d56319e9a399d0aadf1fae0e29b
AC <qingzhi1002@gmail.com>
2025-03-10 11:30:31 +0100

    phone number login instead of email login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Throttle } from '@nestjs/throttler';
+import twilio from 'twilio';
+
+const twilioClient = twilio('ACc5a17d0f28b2892e71f1c1cc8219665a','34b4090d2493dfc90f0297c86f8a55ab');
+const TWILIO_SERVICE_SID = '';
+//sid = 'SKa0ece144300547cf4399e2ed1921c846';
+//api secret = 'jKizLgaHwSKdsRVUJ1LBlUIpx0wDRUmb';
 
 @Controller('auth')
 export class AuthController {
@@ -22,9 +28,18 @@ export class AuthController {
   @Post('login')
   @Throttle({default:{limit: 5, ttl: 60}}) // 5A/60s
   async login(
-    @Body('email') email: string,
+    @Body('phoneNumber') phoneNumber: string,
     @Body('password') password: string
   ){
-    return this.authService.login(email, password);
+    return this.authService.login(phoneNumber, password);
+  }
+
+  @Post('sendCode')
+  async sendCode(@Body('phoneNumber') phoneNumber: string) {
+    try{
+      await twilioClient
+    }catch(error){
+
+    }
   }
 }
